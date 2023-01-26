b2d151bb33a3042d85ed2fbb77ce2b7ffcca19f4
Kiran <kiran@potdar.com>
2023-01-26 16:54:42 +0530

    added date-range-picker, modified calendar with custom styles

--- components/common/Header.tsx
+++ components/common/Header.tsx
@@ -2,12 +2,31 @@ import Image from "next/image";
 import {
   MagnifyingGlassIcon as SearchIcon,
   UserCircleIcon,
-  UsersIcon,
   Bars3Icon as MenuIcon,
 } from "@heroicons/react/24/solid";
 import { GlobeAltIcon } from "@heroicons/react/24/outline";
+import { useState } from "react";
+import "react-date-range/dist/styles.css";
+import "react-date-range/dist/theme/default.css";
+import { DateRangePicker, RangeKeyDict } from "react-date-range";
+// import {} from '@'
 
 function Header() {
+  const [searchInput, setSearchInput] = useState("");
+  const [startDate, setStartDate] = useState(new Date());
+  const [endDate, setEndDate] = useState(new Date());
+
+  const handleCalendarSelect = (ranges: RangeKeyDict) => {
+    setStartDate(ranges.selection.startDate as Date);
+    setEndDate(ranges.selection.endDate as Date);
+  };
+
+  const selectionRange = {
+    key: "selection",
+    startDate,
+    endDate,
+  };
+
   return (
     <header className="sticky top-0 z-50 grid grid-cols-3 bg-white p-5 shadow-md md:px-10">
       <div className="relative flex h-10 cursor-pointer items-center">
@@ -21,6 +40,8 @@ function Header() {
 
       <div className="flex items-center space-x-2 rounded-full border-2 p-2 shadow-md">
         <input
+          value={searchInput}
+          onChange={(e) => setSearchInput(e.target.value)}
           type="text"
           placeholder="Start your search"
           className="flex-grow bg-transparent pl-4 text-sm text-gray-600 placeholder-gray-400 outline-none"
@@ -37,6 +58,21 @@ function Header() {
           <UserCircleIcon className="h-6 cursor-pointer" />
         </div>
       </div>
+
+      {searchInput && (
+        <div className="col-span-3 mx-auto mt-5 flex flex-col">
+          <DateRangePicker
+            ranges={[selectionRange]}
+            minDate={new Date()}
+            rangeColors={["#FD5861"]}
+            onChange={handleCalendarSelect}
+          />
+
+          <div>
+            <h2>Number of guests</h2>
+          </div>
+        </div>
+      )}
     </header>
   );
 }
